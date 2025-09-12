bfb92bc858d66d15082ee21c1eafceefb23c36c1
Arthur Felix de Avila <824148538@ulife.com.br>
2025-09-12 09:59:13 -0300

    merge

--- monolito.js
+++ monolito.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const express = require('express')
-const app = express()
-
-app.use(express.json())
-
-let users = []
-let orders = []
-
-//rota para criar um usuário
-app.post('/users', (req, res) => {
-    const user = req.body
-    users.push(user)
-    res.send({ message: 'User created', user }
-    )
-})
-
-//rota para criar um pedido
-app.post('/orders', (req, res) => {
-    const order = req.body
-    orders.push(order)
-    res.send({ message: 'Order created', order })
-})
-
-//rota para listar todos os pedidos e usarios
-app.get('/dados', (req, res) => {
-    res.send({ users, orders})
-})
-
-//iniciar o servidor na porta 3000
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
-=======
 const express = require('express');
 const app = express();
 
@@ -65,4 +31,3 @@ app.get("/dados", (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
->>>>>>> e4befa3cbc9a9b5c7c518ae5fb02429f2e710cf6
